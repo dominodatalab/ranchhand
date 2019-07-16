3e9d3d30ba46d7b07e01be385fe5db9aee02f09f
Sonny Garcia <sonnygarcia@icloud.com>
2019-07-16 07:02:36 -0400

    bumps deployed rancher version
    v2.2.5 applies an important security update and also provides official
    support for creating k8s v1.14 clusters (double-yay!)

--- pkg/ranchhand/rancher.go
+++ pkg/ranchhand/rancher.go
@@ -39,7 +39,7 @@ var (
 			helm.ReleaseInfo{
 				Name:      "rancher",
 				Namespace: rancherNamespace,
-				Version:   "2.2.4",
+				Version:   "2.2.5",
 				SetValues: map[string]string{
 					"tls":            "external",
 					"privateCA":      "true",
