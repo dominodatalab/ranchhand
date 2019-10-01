489e05fb5f82171c5120207ea080f392cb618493
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2019-09-30 17:07:50 -0700

    Update 2379 Port Access

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -37,7 +37,11 @@ function setup_instance() {
     --instance-name $INSTANCE_NAME
   aws lightsail open-instance-public-ports \
     --port-info fromPort=6443,toPort=6443,protocol=tcp \
+    --instance-name $INSTANCE_NAME 2379
+  aws lightsail open-instance-public-ports \
+    --port-info fromPort=2379,toPort=2379,protocol=tcp \
     --instance-name $INSTANCE_NAME
+
 
   local ipaddr=$(aws lightsail get-instance --instance-name $INSTANCE_NAME \
     | jq --raw-output '.instance | .publicIpAddress')
