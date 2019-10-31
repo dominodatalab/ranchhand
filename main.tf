9d6fe77077521c4f42a991d1422f02bc7257a889
Zane Silver <zanesilver@Zanes-MacBook-Pro.local>
2019-10-31 08:41:36 -0700

    Unique Ansible Directory w/o Timestamp

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 locals {
-  ip_addresses = join(",", var.node_ips)
+  ip_addresses      = join(",", var.node_ips)
   ansbile_ssh_proxy = var.ssh_proxy_host == "" ? "" : format("-o StrictHostKeyChecking=no -o ProxyCommand=\"ssh -i %s -o StrictHostKeyChecking=no -W %%h:%%p -q %s@%s\"", var.ssh_key_path, var.ssh_proxy_user, var.ssh_proxy_host)
-  cert_dnsnames = format("DNS:%s", join(",DNS:", var.cert_dnsnames))
-  cert_ipaddresses = length(var.cert_ipaddresses) == 0 ? "" : format(",IP:%s", join(",IP:", var.cert_ipaddresses))
-  cert_names = format("%s%s", local.cert_dnsnames, local.cert_ipaddresses)
-  working_dir = format("%s/ranchhand-ansible.%s", var.working_dir, formatdate("YYYYMMDDhhmmss", timestamp()))
+  cert_dnsnames     = format("DNS:%s", join(",DNS:", var.cert_dnsnames))
+  cert_ipaddresses  = length(var.cert_ipaddresses) == 0 ? "" : format(",IP:%s", join(",IP:", var.cert_ipaddresses))
+  cert_names        = format("%s%s", local.cert_dnsnames, local.cert_ipaddresses)
+  working_dir       = "${var.working_dir}/${random_id.ansible_dir.b64_std}"
+}
+
+resource "random_id" "ansible_dir" {
+  byte_length = 8
+  prefix      = "ranchhand-ansible."
 }
 
 resource "random_password" "password" {
@@ -16,13 +21,13 @@ resource "random_password" "password" {
 }
 
 resource "local_file" "current_work_directory" {
-    filename = "${local.working_dir}/.ansible"
+  filename = "${local.working_dir}/.ansible"
 }
 
 resource "local_file" "copy_kubeconfig" {
-    content = file("${local.working_dir}/kube_config_rancher-cluster.yml")
-    filename = "${var.working_dir}/kube_config_rancher-cluster.yml"
-    depends_on = ["null_resource.ansible_playbook"]
+  content    = file("${local.working_dir}/kube_config_rancher-cluster.yml")
+  filename   = "${var.working_dir}/kube_config_rancher-cluster.yml"
+  depends_on = ["null_resource.ansible_playbook"]
 }
 
 resource "null_resource" "ansible_playbook" {
@@ -37,7 +42,7 @@ resource "null_resource" "ansible_playbook" {
       "-e 'local_output_dir=${local.working_dir}'",
       "ansible/prod.yml --diff"
     ])
-    
+
     working_dir = "${path.module}"
     environment = {
       RANCHER_PASSWORD = var.admin_password == "" ? join("", random_password.password.*.result) : var.admin_password
