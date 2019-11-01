3dbcdbccf63da50071e4b392342a5d18140164b0
zs-ddl <54289772+zs-ddl@users.noreply.github.com>
2019-11-01 08:37:39 -0700

    Unique Ansible Directory w/o Timestamp (#39)

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 locals {
-  ip_addresses = join(",", var.node_ips)
-  ansbile_ssh_proxy = var.ssh_proxy_host == "" ? "" : format("-o StrictHostKeyChecking=no -o ProxyCommand=\"ssh -i %s -o StrictHostKeyChecking=no -W %%h:%%p -q %s@%s\"", var.ssh_key_path, var.ssh_proxy_user, var.ssh_proxy_host)
-  cert_dnsnames = format("DNS:%s", join(",DNS:", var.cert_dnsnames))
-  cert_ipaddresses = length(var.cert_ipaddresses) == 0 ? "" : format(",IP:%s", join(",IP:", var.cert_ipaddresses))
-  cert_names = format("%s%s", local.cert_dnsnames, local.cert_ipaddresses)
-  working_dir = format("%s/ranchhand-ansible.%s", var.working_dir, formatdate("YYYYMMDDhhmmss", timestamp()))
+  ip_addresses      = join(",", var.node_ips)
+  ansible_ssh_proxy = var.ssh_proxy_host == "" ? "" : format("-o StrictHostKeyChecking=no -o ProxyCommand=\"ssh -i %s -o StrictHostKeyChecking=no -W %%h:%%p -q %s@%s\"", var.ssh_key_path, var.ssh_proxy_user, var.ssh_proxy_host)
+  cert_dnsnames     = format("DNS:%s", join(",DNS:", var.cert_dnsnames))
+  cert_ipaddresses  = length(var.cert_ipaddresses) == 0 ? "" : format(",IP:%s", join(",IP:", var.cert_ipaddresses))
+  cert_names        = format("%s%s", local.cert_dnsnames, local.cert_ipaddresses)
 }
 
 resource "random_password" "password" {
@@ -15,14 +14,10 @@ resource "random_password" "password" {
   override_special = "!@#$%&*()_=+[]{}<>:?"
 }
 
-resource "local_file" "current_work_directory" {
-    filename = "${local.working_dir}/.ansible"
-}
-
 resource "local_file" "copy_kubeconfig" {
-    content = file("${local.working_dir}/kube_config_rancher-cluster.yml")
-    filename = "${var.working_dir}/kube_config_rancher-cluster.yml"
-    depends_on = ["null_resource.ansible_playbook"]
+  content    = file("${var.working_dir}/ansible.${null_resource.ansible_playbook.id}/kube_config_rancher-cluster.yml")
+  filename   = "${var.working_dir}/kube_config_rancher-cluster.yml"
+  depends_on = ["null_resource.ansible_playbook"]
 }
 
 resource "null_resource" "ansible_playbook" {
@@ -32,16 +27,15 @@ resource "null_resource" "ansible_playbook" {
       "-i '${local.ip_addresses},'",
       "--private-key=${var.ssh_key_path}",
       "--user=${var.ssh_username}",
-      "--ssh-common-args='-o StrictHostKeyChecking=no ${local.ansbile_ssh_proxy}'",
+      "--ssh-common-args='-o StrictHostKeyChecking=no ${local.ansible_ssh_proxy}'",
       "-e 'cert_names=${local.cert_names}'",
-      "-e 'local_output_dir=${local.working_dir}'",
+      "-e 'local_output_dir=${var.working_dir}/ansible.${self.id}'",
       "ansible/prod.yml --diff"
     ])
-    
+
     working_dir = "${path.module}"
     environment = {
       RANCHER_PASSWORD = var.admin_password == "" ? join("", random_password.password.*.result) : var.admin_password
     }
   }
-  depends_on = ["local_file.current_work_directory"]
 }
