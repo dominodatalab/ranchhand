bb88aba868bd9b74d14a7c9d362a7cdc983962bc
Saahil Dhulla <saahildhulla@Saahils-MacBook-Pro.local>
2019-12-13 15:39:42 -0800

    adding in environment variable in ansible to be picked up and propagated to rancher chart during installation

--- main.tf
+++ main.tf
@@ -23,7 +23,8 @@ resource "null_resource" "ansible_playbook" {
         --user=${var.ssh_username} \
         --ssh-common-args='-o StrictHostKeyChecking=no ${local.ansible_ssh_proxy}' \
         -e 'cert_names=${local.cert_names}' \
-        -e 'local_output_dir=${var.working_dir}/ansible.${self.id}' \
+        -e ip_addr_count='${#local.ip_addresses[@]}'
+	-e 'local_output_dir=${var.working_dir}/ansible.${self.id}' \
         ansible/prod.yml --diff
     EOF
 
