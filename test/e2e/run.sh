8184a0233e4c25efc0a87f88c2226c877eb4e76e
Steven Davidovitz <steved@users.noreply.github.com>
2020-10-12 22:59:38 -0700

    make the detection for when rancher is "up" a bit more reliable (#54)
    * make the detection for when rancher is "up" a bit more reliable
    
    * The login check sometimes failed due to rancher still starting up
    which would cause the change password action to not run.
    
    * code review comments
    
    * test out e2e testing
    
    * switch to upstream python image
    
    * remove wget & zip install
    
    * sudo unzip
    
    * remove jq usage
    
    * fix ubuntu_18_04 blueprint id
    
    * update SSH files
    
    * revert back to json_query
    
    * create a rancher_password by default
    
    * fixme...
    
    * revert password.yml changes
    
    * revert password.yml changes
    
    * revert CI changes

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -3,7 +3,7 @@ set -ex
 
 INSTANCE_NAME="${INSTANCE_NAME:-"ranchhand-local-$USER"}"
 INSTANCE_BLUEPRINT_ID="${INSTANCE_BLUEPRINT_ID:-ubuntu_16_04_2}"
-SSH_KEY_FILE="${SSH_KEY_FILE:-${HOME}/.ssh/id_rsa_5a19aee7ef984f2f68c3be7262f91d35}"
+SSH_KEY_FILE="${SSH_KEY_FILE:-${HOME}/.ssh/id_rsa_a4d238e594137d6a2ec652c68f7f0e6b}"
 SSH_USER="${SSH_USER:-ubuntu}"
 
 function setup_instance() {
@@ -25,7 +25,7 @@ function setup_instance() {
   for retries in $(seq 0 ${max_retries}); do
     sleep 10
 
-    local state=$(aws lightsail get-instance-state --instance-name $INSTANCE_NAME | jq -r '.state.name')
+    local state=$(aws lightsail get-instance-state --instance-name $INSTANCE_NAME --query 'state.name' --output text)
     if [[ $state == "running" ]]; then
       break
     fi
@@ -35,12 +35,10 @@ function setup_instance() {
 
   if [ "$retries" -eq "${max_retries}" ]; then echo "$INSTANCE_NAME is not ready!"; exit 5; fi
 
-  local ipaddr=$(aws lightsail get-instance --instance-name $INSTANCE_NAME \
-    | jq --raw-output '.instance | .publicIpAddress')
+  local ipaddr=$(aws lightsail get-instance --instance-name $INSTANCE_NAME --query 'instance.publicIpAddress' --output text)
   echo $ipaddr > instance-ip
 
-  local private_ipaddr=$(aws lightsail get-instance --instance-name $INSTANCE_NAME \
-    | jq --raw-output '.instance | .privateIpAddress')
+  local private_ipaddr=$(aws lightsail get-instance --instance-name $INSTANCE_NAME --query 'instance.privateIpAddress' --output text)
   echo $private_ipaddr > private-instance-ip
 
   for retries in $(seq 0 ${max_retries}); do
